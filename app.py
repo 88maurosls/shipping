835277fad0f040e7f13229e9d4abdfcd345807d0
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-13 18:20:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ if uploaded_file is not None:
     # Lettura del file caricato
     df = pd.read_csv(uploaded_file, delimiter=';')
 
+    # Leggi il file countrycode.txt e crea un dizionario
+    try:
+        countrycode_df = pd.read_csv('countrycode.txt', delimiter=';', header=None)
+        countrycode_dict = dict(zip(countrycode_df[0], countrycode_df[2]))
+    except Exception as e:
+        st.error(f"Errore nella lettura di countrycode.txt: {e}")
+        countrycode_dict = {}
+
     # Identifica le righe con COSTI_SPEDIZIONE diversi da 0
     costs_rows = df[df[' COSTI_SPEDIZIONE'] != 0]
 
@@ -20,7 +28,14 @@ if uploaded_file is not None:
 
     # Apporta le modifiche necessarie
     adjusted_rows = unique_costs_rows.copy()
-    adjusted_rows[' PREZZO_1'] = adjusted_rows[' COSTI_SPEDIZIONE']
+    for index, row in adjusted_rows.iterrows():
+        nazione = row[' NAZIONE']
+        if nazione in countrycode_dict:
+            iva = countrycode_dict[nazione]
+            costo_spedizione = row[' COSTI_SPEDIZIONE']
+            costo_senza_iva = costo_spedizione - (costo_spedizione * iva / 100)
+            adjusted_rows.at[index, ' PREZZO_1'] = costo_senza_iva
+
     adjusted_rows[' COD_ART'] = adjusted_rows[' COSTI_SPEDIZIONE'].apply(lambda x: f"SHIPPINGCOSTS{x}")
     adjusted_rows[' COD_ART_DOC'] = adjusted_rows[' COD_ART']
     adjusted_rows[' DESCR_ART'] = "Shipping Costs"
@@ -46,4 +61,3 @@ if uploaded_file is not None:
         mime='text/csv',
     )
     st.balloons()
-
