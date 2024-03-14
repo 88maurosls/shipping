29f9520727b948e0346dff254dcb12522fc0fc33
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-14 16:15:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ st.title('Modifica File CSV per Costi di Spedizione e IVA')
 uploaded_file = st.file_uploader("Carica il file CSV", type='csv')
 
 if uploaded_file is not None:
-    # Lettura del file caricato con sostituzione della virgola con il punto nei numeri
+    # Lettura del file caricato
     df = pd.read_csv(uploaded_file, delimiter=';')
-    df[' PREZZO_1'] = df[' PREZZO_1'].str.replace(',', '.').astype(float)
-    df[' COSTI_SPEDIZIONE'] = df[' COSTI_SPEDIZIONE'].str.replace(',', '.').astype(float)
+
+    # Converti le colonne in stringhe e poi sostituisci la virgola con il punto
+    df[' PREZZO_1'] = df[' PREZZO_1'].astype(str).str.replace(',', '.').astype(float)
+    if df[' COSTI_SPEDIZIONE'].dtype != 'float64':
+        df[' COSTI_SPEDIZIONE'] = df[' COSTI_SPEDIZIONE'].astype(str).str.replace(',', '.').astype(float)
 
     # Leggi il file countrycode.txt e crea un dizionario
     try:
@@ -59,12 +62,12 @@ if uploaded_file is not None:
     # Ordina il dataframe finale per NUM_DOC
     final_df.sort_values(by=[' NUM_DOC'], inplace=True)
 
-    # Converti i numeri in stringhe con virgole per i decimali prima di esportare
-    final_df[' PREZZO_1'] = final_df[' PREZZO_1'].apply(lambda x: '{:.2f}'.format(x).replace('.', ','))
-    final_df[' COSTI_SPEDIZIONE'] = final_df[' COSTI_SPEDIZIONE'].apply(lambda x: '{:.2f}'.format(x).replace('.', ','))
+    # Prima di esportare, converti di nuovo i numeri in stringhe con virgole per i decimali
+    df[' PREZZO_1'] = df[' PREZZO_1'].apply(lambda x: '{:.2f}'.format(x).replace('.', ','))
+    df[' COSTI_SPEDIZIONE'] = df[' COSTI_SPEDIZIONE'].apply(lambda x: '{:.2f}'.format(x).replace('.', ','))
 
     # Converti il dataframe finale in CSV
-    csv = final_df.to_csv(sep=';', index=False, float_format='%.2f')
+    csv = df.to_csv(sep=';', index=False, float_format='%.2f')
 
     # Bottone per il download del file modificato
     st.download_button(
