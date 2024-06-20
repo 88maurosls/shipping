914268b03aa899aaa1075661ca4af86b63d67496
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-06-20 09:41:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ def process_shipping_rows(rows, countrycode_dict):
         nazione = row[' NAZIONE']
         if nazione in countrycode_dict:
             iva = countrycode_dict[nazione]
-            costo_spedizione = float(row[' COSTI_SPEDIZIONE'])
             try:
-                # Assegna il risultato del calcolo a costo_senza_iva
+                # Rimuovi spazi bianchi, sostituisci virgole con punti e converti a float
+                costo_spedizione = float(row[' COSTI_SPEDIZIONE'].strip().replace(',', '.'))
                 costo_senza_iva = costo_spedizione / (1 + iva / 100)
-                # Ora puoi arrotondare costo_senza_iva senza errori
                 formatted_price = round(costo_senza_iva, 2)
                 adjusted_rows.at[index, ' PREZZO_1'] = formatted_price
+            except ValueError as ve:
+                errors.append(f"Valore non valido per COSTI_SPEDIZIONE nella riga {index + 1}: {row[' COSTI_SPEDIZIONE']} - {ve}")
             except Exception as e:
                 errors.append(f"Errore nella riga {index + 1}: {e}")
         else:
             adjusted_rows.at[index, ' PREZZO_1'] = row[' COSTI_SPEDIZIONE']
             
-
     # Stampa gli errori
     for error in errors:
         st.error(error)
@@ -36,14 +36,10 @@ def process_shipping_rows(rows, countrycode_dict):
     adjusted_rows[' HSCODE'] = ""  # Lascia vuota la colonna HSCODE
     return adjusted_rows
 
-
-
 # Funzione per l'elaborazione delle righe dell'IVA
 def process_vat_rows(rows, countrycode_dict, df_original):
-    # Escludi la nazione "86" ITALIA dalle righe da elaborare
     vat_rows = rows.copy()
     vat_rows = vat_rows[vat_rows[' NAZIONE'].astype(str) != "86"]
-
     vat_rows = vat_rows[vat_rows[' NAZIONE'].isin(countrycode_dict.keys())]
 
     for index, row in vat_rows.iterrows():
@@ -76,7 +72,6 @@ def process_vat_rows(rows, countrycode_dict, df_original):
     vat_rows[' HSCODE'] = ""
     return vat_rows
 
-
 # Titolo dell'applicazione Streamlit
 st.title('Modifica File CSV per Costi di Spedizione e IVA')
 
@@ -84,10 +79,8 @@ st.title('Modifica File CSV per Costi di Spedizione e IVA')
 uploaded_file = st.file_uploader("Carica il file CSV", type='csv')
 
 if uploaded_file is not None:
-    # Lettura del file caricato
     df = pd.read_csv(uploaded_file, delimiter=';')
 
-    # Leggi il file countrycode.txt e crea un dizionario
     try:
         countrycode_df = pd.read_csv('countrycode.txt', delimiter=';', header=None)
         countrycode_dict = dict(zip(countrycode_df[0], countrycode_df[2]))
@@ -95,27 +88,14 @@ if uploaded_file is not None:
         st.error(f"Errore nella lettura di countrycode.txt: {e}")
         countrycode_dict = {}
 
-    # Identifica le righe con COSTI_SPEDIZIONE diversi da 0
     costs_rows = df[df[' COSTI_SPEDIZIONE'] != 0]
-
-    # Filtra le righe uniche basate su NUM_DOC
     unique_costs_rows = costs_rows.drop_duplicates(subset=[' NUM_DOC'])
-
-    # Elabora le righe delle spedizioni
     adjusted_rows = process_shipping_rows(unique_costs_rows, countrycode_dict)
-
-    # Aggiungi le righe degli Shipping Costs al dataframe originale
     df_with_shipping = pd.concat([df, adjusted_rows], ignore_index=True)
-
-    # Elabora le righe dell'IVA
     vat_rows = process_vat_rows(unique_costs_rows, countrycode_dict, df_with_shipping)
-
-    # Aggiungi le righe dell'IVA al dataframe
     final_df = pd.concat([df_with_shipping, vat_rows], ignore_index=True)
 
-    # Rimuovi l'IVA dai 'PREZZO_1' dove necessario e se 'PARTITA_IVA' è vuoto
     for index, row in final_df.iterrows():
-        # Verifica se 'PARTITA_IVA' è NaN o vuoto
         partita_iva_is_empty = pd.isna(row[' PARTITA_IVA']) or (isinstance(row[' PARTITA_IVA'], str) and not row[' PARTITA_IVA'].strip())
 
         if row[' NAZIONE'] in countrycode_dict and partita_iva_is_empty:
@@ -127,21 +107,14 @@ if uploaded_file is not None:
             except Exception as e:
                 st.error(f"Errore nella rimozione dell'IVA da 'PREZZO_1' per la riga {index}: {e}")
 
-
-
-
-    # Ordina il dataframe finale per NUM_DOC e poi per PROGRESSIVO_RIGA
     final_df.sort_values(by=[' NUM_DOC', ' PROGRESSIVO_RIGA'], inplace=True)
 
-    # Assegna una nuova sequenza numerica a PROGRESSIVO_RIGA mantenendo uguali i valori per righe identiche
     new_progressivo = (final_df.groupby([' NUM_DOC', ' PROGRESSIVO_RIGA'])
                       .ngroup() + 1)
     final_df[' PROGRESSIVO_RIGA'] = new_progressivo
 
-    # Converti il dataframe finale in CSV
     csv = final_df.to_csv(sep=';', index=False, float_format='%.2f').encode('utf-8').decode('utf-8').replace('.', ',').encode('utf-8')
 
-    # Bottone per il download del file modificato
     st.write("Anteprima dei dati:", final_df)
     st.download_button(
         label="Scarica il CSV modificato",
